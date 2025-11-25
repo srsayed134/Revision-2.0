139589b97ae77925a209b219728e0d950cb9c31b
srsayed134 <srsayed134@gmail.com>
2025-11-25 20:32:22 +0600

    Type conversion

---
+++ JS/new.js
@@ -0,0 +1,141 @@
+// 01:-Basic
+// console.log(2 + 2) ;
+// console.log({name: "Alex"})
+
+// Variables---------
+//Declared variable;
+// let name;
+//Assin value;----------
+// name = 5;
+// console.log(name)
+
+//Challenge++++++++++++++
+// let name = "Sayed";
+// let whatDoYouWannaBecomeInYourLife = "programmer";
+// let gender = "Male";
+// let twitterHandle = "@sayed";
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterHandle);
+
+// Primitive Types
+// 1. Number-----------
+
+// let num = 10;
+// let num1 = 15;
+// // console.log(num);
+// console.log(typeof num); //number
+// console.log(num + num1); //25;
+// console.log(2 + 1); //3
+
+// let counter = 0;
+// counter++;
+// counter++;
+// console.log(counter) //2;
+// counter--;
+// console.log(counter) //1;
+
+//Challenge++++++++++++++++++;
+// let firstFavNum = 7;
+// let secondFavName = 28;
+// let res1 = firstFavNum + secondFavName;
+// console.log(res1)//35;
+// let res2 = firstFavNum - secondFavName;
+// console.log(res2); // -21
+// let res3 = firstFavNum * secondFavName;
+// console.log(res3); //196
+// let res4 = firstFavNum / secondFavName;
+// console.log(res4); // 0.25;
+// let res5 = firstFavNum % secondFavName;
+// console.log(res5); // 7
+// let res6 = firstFavNum ** secondFavName;
+// console.log(res6); // 4.59998;
+
+// 2. Boolean ----------------
+
+// let isTrue = true;
+// console.log(isTrue);
+// let isFalse = false;
+// console.log(isFalse);
+// let isTrue = NaN;
+// console.log(isTrue);
+
+// let number = 10;
+// console.log(number + undefined); //NaN;
+
+// let notDefined = null;
+// console.log(notDefined); //Null is set by programmer;
+
+// let notDefined1;
+// console.log(notDefined1) //undefined is set by compiler;
+
+//Challenge ++++++++++++;
+
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 28;
+// let add = favNumb + undefined;
+// console.log(add); //NaN;
+
+// 3. Comparison Operators---------------
+// Relational operators;
+// let num1 = 10;
+// let num2 = 15;
+// let num3 = 10;
+
+
+// let res = num1 > num2;
+// let res1 = num1 < num2;
+// let res2 = num1 >= num3;
+// let res3 = num1 <= num3;
+// console.log(res) //false
+// console.log(res1) //true
+// console.log(res2) //true
+// console.log(res3) //true
+
+// Equality operators;
+// let num1 = 10;
+// let num2 = "10";
+// let num3 = 15;
+// let num4 = "15";
+
+// let res = num1 === num2;
+// let res1 = num1 == num2;
+// let res2 = num1 !== num3;
+// let res3 = num3 != num4;
+// console.log(res) //false
+// console.log(res1) //true;
+// console.log(res2) //true;
+// console.log(res3) //false;
+
+// 3.Strings ------------;
+//Challenge +++++++++++++++++;
+
+// let favActorFastName = "Salman";
+// let favActorLastName = "Khan"
+
+// let fullName = favActorFastName.concat(favActorLastName);
+// console.log(fullName);
+// let capitalize = fullName.toUpperCase();
+// console.log(capitalize);
+// let message = `My favorite Actor Is ${capitalize} & say something about your fav actor`;
+
+// let appendMessage = message += ` his best show is silicon valley`;
+// console.log(appendMessage);
+
+// 4. Type Conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
