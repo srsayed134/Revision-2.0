4d38cd1f0517a22e62233a089dd600227a47f7e9
srsayed134 <srsayed134@gmail.com>
2025-12-08 11:12:06 +0600

    Random emojis

---
+++ JS/Project/Emoji change/app.js
@@ -0,0 +1,7 @@
+const emoji = document.querySelector("#emoji");
+
+const emojis = ["😂", "😒","😍","😉","😩","😐","🥰","😊","🤣"];
+
+emoji.addEventListener(("mouseover"),() => {
+    emoji.innerHTML = emojis[Math.floor(Math.random ()* emojis.length)]
+});
