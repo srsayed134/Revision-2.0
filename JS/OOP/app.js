93e2ed9729bab8e3c8d90fbb98b8d5104c42dc81
srsayed134 <srsayed134@gmail.com>
2025-12-11 22:57:10 +0600

    Practice object creat and prototypes

--- JS/OOP/app.js
+++ JS/OOP/app.js
@@ -129,3 +129,128 @@
 // const Light = new PersonProfile("Light", "Yagami", 22); //this new keyword make "this" an self objesct;
 // console.log(Light)
 // console.log(Light.info())
+
+//Challemge+++++++++++++++++++++
+
+// function Car(make, model, year, color){
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//     this.color = year;
+//     this.start = function(){
+//         return `Starting the ${this.make} ${this.model}... `
+//     };
+//     this.stop = function(){
+//         return `Stopping the ${this.make} ${this.model}`
+//     }
+// }
+
+// const car1 = new Car("Toyota", "Camry", 2016, "Silver")
+// const car2 = new Car("Honda", "C-rv", 2017, "Black")
+
+// console.log(car1.start())
+// console.log(car2.start())
+
+//++++++++++++ Built in Constructure+++++++++++++++
+
+// const num1 = new Number(10);
+// const num2 = 20;
+// const str1 = new String("huxn webdeb");
+// const bool1 = new Boolean(false);
+// const arr1 = new Array(1, 2, 3);
+
+// console.log(typeof num1)
+// console.log(typeof num2)
+// console.log(typeof str1);
+// console.log(typeof arr1);
+// console.log(str1);
+// console.log(arr1);
+
+//+++++++++++++++Object.create() method+++++++++++
+// let person = {
+//     greet: function(){
+//         return `Hello my name is ${this.fname} ${this.lname}`
+//     }
+// }
+
+// //---------------------------
+// const jhon = Object.create(person);
+// jhon.fname = "Jhon";
+// jhon.lname = "Doe";
+
+// console.log(jhon.fname)
+// console.log(jhon.lname)
+// console.log(jhon.greet());
+
+// //---------------------------
+// let charlie = Object.create(person, {
+//     fname: {value: "Charlie"},
+//     lname: {value: "Abdoe"},
+// })
+
+// console.log(charlie.fname)
+// console.log(charlie.lname)
+// console.log(charlie.greet())
+
+//+++++++++++++Prototypes+++++++++++
+
+// const obj = {};
+// console.log(obj.__proto__);
+// console.log(obj.constructor.prototype);
+// console.log(Object.getPrototypeOf(obj))
+
+// const arr = new Array();
+// console.log(arr)
+
+//--------------------------
+
+// function Person(firstName, lastName, pl){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.pl = pl;
+// }
+
+// const Jhon = new Person("Jhon", "Doe", "JS");
+// console.log(Jhon.__proto__); 
+
+//--------------------------
+// Array.prototype.pop = function (){
+//     return "Pop is not working"
+// }
+
+// const myArr = ["One", "Two", "Three"];
+// console.log(myArr.pop())
+//--------------------------
+
+// String.prototype.hello = function () {
+//     console.log(this);
+//     console.log(this.toUpperCase());
+// };
+
+// console.log("hello and welcome".hello());
+
+//+++++++++++++++++Prototypal inheritance++++++++++++(left over)
+
+// function Animal(name){
+//     this.name = name;
+// }
+
+// Animal.prototype.sound = function() {
+//     return "Animal Sound";
+// }
+
+// const animal1 = new Animal("Frog");
+// console.log(animal1)
+// console.log(animal1.name) //Frog
+// console.log(animal1.sound()); //Animal sound
+
+// function Dog(name, breed) {
+//     this.name = name;
+//     this.breed = breed;
+// }
+
+// const dog1 = new Dog("Buddy", "Genus");
+// console.log(dog1)
+
+//+++++++++++++++Classes +++++++++++++++++
+
