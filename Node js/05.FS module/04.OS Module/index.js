52ffb22a0cf947ba6526a63293ad8e314761939b
srsayed134 <srsayed134@gmail.com>
2026-03-07 11:05:13 +0600

    Learn os module

---
+++ Node js/05.FS module/04.OS Module/index.js
@@ -0,0 +1,9 @@
+import os from "os";
+// console.log(os.platform()); -> win32
+// console.log(os.arch())
+console.log(os.cpus())
+console.log(os.hostname())
+console.log(os.homedir())
+console.log(os.networkInterfaces())
+console.log(os.freemem())
+console.log(os.totalmem())
